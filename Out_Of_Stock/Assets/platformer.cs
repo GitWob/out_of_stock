82adfab33911db834cc87b9c1f4fb194caa8d409
mls8504 <mls8504@rit.edu>
2021-05-21 22:20:21 -0400

    player movement left and right

---
+++ Out_Of_Stock/Assets/platformer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformer : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+       float x = Input.GetAxisRaw("Horizontal");
+       float moveBy = x * speed;
+        rb.velocity = new Vector2(moveBy, rb.velocity.y);
+        
+    }
+}
