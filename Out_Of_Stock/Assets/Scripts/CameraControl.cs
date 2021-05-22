1f735123ee76c8319a0c4afd1614bfeac4f30eb6
meo6332 <60484806+meo6332@users.noreply.github.com>
2021-05-22 17:29:15 -0400

    Updated Camera Controls Script
    Made camera control script allowing the main camera to follow the player.  Also added the beta box and ground to the prefabs.

--- Out_Of_Stock/Assets/Scripts/CameraControl.cs
+++ Out_Of_Stock/Assets/Scripts/CameraControl.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
+    public Transform player;
+    public float cameraDistance = 30.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,17 @@ public class CameraControl : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void Awake()
+    {
+        int sHeight = Screen.height;
+        GetComponent<UnityEngine.Camera>().orthographicSize = ((sHeight / 2) / cameraDistance);
+    }
+
+    private void FixedUpdate()
+    {
+
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
     }
 }
