e06f12281add358e726de0cc9bc1631333cad413
Will O'Brien <usefulinternetmailbox@gmail.com>
2021-05-21 23:17:47 -0400

    Renamed Scripts And Started Gun Control

--- Out_Of_Stock/Assets/Scripts/CameraControl.cs
+++ Out_Of_Stock/Assets/Scripts/CameraControl.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControl : MonoBehaviour
+public class CameraControl : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
---
+++ Out_Of_Stock/Assets/Scripts/GunControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunControl : MonoBehaviour
+{
+    public Camera cam;
+    public GameObject gun;
+    public GunFiring gunFiring;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        gunFiring = gun.GetComponent<GunFiring>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        gun.transform.LookAt(cam.ScreenToWorldPoint(Input.mousePosition));
+    }
+}
