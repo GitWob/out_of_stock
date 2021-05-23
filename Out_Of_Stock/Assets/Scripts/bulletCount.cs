af2f06f8e913ef9f75fe0a6f1f7e46ecf10568a4
mls8504 <mls8504@rit.edu>
2021-05-23 16:49:33 -0400

    its red now maybe

--- Out_Of_Stock/Assets/Scripts/bulletCount.cs
+++ Out_Of_Stock/Assets/Scripts/bulletCount.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class bulletCount : MonoBehaviour
 {
     public GunControl playerGunControl;
+    private GUIStyle red;
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +22,9 @@ public class bulletCount : MonoBehaviour
 
     private void OnGUI()
     {
-        GUI.Label(new Rect(40, 40, 100, 20), "Bullets left: " + playerGunControl.bulletCount,"black");
+        red = new GUIStyle();
+        red.normal.textColor = Color.red;
+        GUI.contentColor = Color.red;
+        GUI.Label(new Rect(40, 40, 100, 20), "Bullets left: " + playerGunControl.bulletCount,red);
     }
 }
