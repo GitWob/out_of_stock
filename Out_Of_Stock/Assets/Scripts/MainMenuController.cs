dab81bb80e4906d842aa47325b7a545e263cce54
mls8504 <mls8504@rit.edu>
2021-05-22 22:25:55 -0400

    Main Menu and Credits menu working!

---
+++ Out_Of_Stock/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public GameObject Main_Menu;
+    public GameObject credits;
+
+    void Start()
+    {
+        credits.SetActive(false);
+    }
+    public void playGame()
+    {
+        SceneManager.LoadScene("WillScene");
+    } 
+
+    public void Credits()
+    {
+        Main_Menu.SetActive(false);
+        credits.SetActive(true);
+    } 
+
+    public void back()
+    {
+        Main_Menu.SetActive(true);
+        credits.SetActive(false);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
