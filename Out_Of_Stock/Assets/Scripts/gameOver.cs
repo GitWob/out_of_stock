999ed8d693ac7c3112f2413ba72beef231054cf6
Will O'Brien <usefulinternetmailbox@gmail.com>
2021-05-23 16:53:34 -0400

    Made Collision Check for Soup

--- Out_Of_Stock/Assets/Scripts/gameOver.cs
+++ Out_Of_Stock/Assets/Scripts/gameOver.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class gameOver : MonoBehaviour
 {
+    public GameObject player;
+    private BoxCollider2D playerCollider;
+    private BoxCollider2D soupCollider;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerCollider = player.GetComponent<BoxCollider2D>();
+        soupCollider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Collision())
+        {
+            // game won! put level code here
+        }
+    }
+
+    bool Collision()
+    {
+        if (soupCollider.bounds.Intersects(playerCollider.bounds))
+        {
+            return true;
+        }
+        return false;
     }
 }
