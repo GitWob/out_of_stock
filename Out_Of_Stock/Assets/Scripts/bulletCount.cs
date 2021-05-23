e3fb2563a62d83d21115be997b8abe31c02b93d3
Will O'Brien <usefulinternetmailbox@gmail.com>
2021-05-23 13:18:25 -0400

    Bullet Counter On Screen

--- Out_Of_Stock/Assets/Scripts/bulletCount.cs
+++ Out_Of_Stock/Assets/Scripts/bulletCount.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class bulletCount : MonoBehaviour
 {
+    public GunControl playerGunControl;
+
     // Start is called before the first frame update
     void Start()
     {
-       
 
     }
 
     // Update is called once per frame
     void Update()
     {
-      
+        
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(40, 40, 100, 20), "Bullets left: " + playerGunControl.bulletCount);
     }
 }
